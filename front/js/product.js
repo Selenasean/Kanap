4bb5b8821a34cbad62bd7e5cf9f2d67343a29981
Selena Sean <selena.sean.1994@gmail.com>
2022-07-14 15:52:26 +0200

    Page produit terminée - Ajout des produits au localStorage

---
+++ front/js/product.js
@@ -0,0 +1,110 @@
+/* Get the query string of the current page's URL */
+const queryString_URLIdProduct = window.location.search;
+console.log(queryString_URLIdProduct);
+
+/**
+ * Get the interested data = product's ID = idProduct
+ * @constructor { URLSearchParams() }
+ * @param { url }*/
+
+let urlParamSearch = new URLSearchParams(queryString_URLIdProduct);
+let idProduct = urlParamSearch.get("id");
+console.log(idProduct);
+
+/**
+ *  GET one specific product's data from API
+ * @async function to get one product's data from API using fetch
+ * @return { promise } which is parse/translate into JS in the function
+ */
+
+async function getOneProduct() {
+  const urlOneProduct = `http://localhost:3000/api/products/${idProduct}`;
+  const oneProduct = await fetch(urlOneProduct);
+  const oneProductParse = await oneProduct.json();
+  return oneProductParse;
+}
+
+/**
+ * @async function to get product's data
+ * Display product's data on the page via DOM
+ */
+
+(async function main() {
+  try {
+    const oneProduct = await getOneProduct();
+    console.log(oneProduct);
+
+    // Insert oneProduct's settings in DOM //
+
+    // Creation <img> //
+    const createImg = document.createElement("img");
+    createImg.setAttribute("src", oneProduct.imageUrl);
+    createImg.setAttribute("alt", oneProduct.description);
+    document.querySelector(".item__img").appendChild(createImg);
+
+    // Creation innerText in <h1> //
+    document.querySelector("#title").innerText = oneProduct.name;
+
+    // Creation innertext in <span> //
+    document.querySelector("#price").innerText = oneProduct.price;
+
+    // Creation innerTexte in <p> //
+    document.querySelector("#description").innerText = oneProduct.description;
+
+    // Creation input's colors choice //
+    for (let color of oneProduct.colors) {
+      const createOption = document.createElement("option");
+      createOption.setAttribute("value", color);
+      createOption.innerText = `${color}`;
+      document.querySelector("#colors").appendChild(createOption);
+    }
+  } catch (err) {
+    console.log("hoho error");
+  }
+})();
+
+/**
+ * Storage data in localStorage for cart
+ * @function saveInLocalStorage which save as an array the product in localStorage
+ * @param product we want to add*/
+
+function saveInLocalStorage(productOptions) {
+  let productInLocalStorage = JSON.parse(localStorage.getItem("sofa")); // Get the product in array as an object
+
+  // If array in localStorage is empty = null, add the productOption'array in it, as a string
+  if (productInLocalStorage === null) {
+    productInLocalStorage = [];
+    productInLocalStorage.push(productOptions);
+    localStorage.setItem("sofa", JSON.stringify(productInLocalStorage));
+  } else {
+    // Else find in localStorage's array if there is a product with same id & same color as the product we want to add
+    let foundProduct = productInLocalStorage.find(
+      (p) => p.id == productOptions.id && p.color == productOptions.color
+    );
+    if (foundProduct === undefined) {
+      // If there isn't same id & color at all, add productOptions in localStorage as a string
+      productInLocalStorage.push(productOptions);
+      localStorage.setItem("sofa", JSON.stringify(productInLocalStorage));
+    } else {
+      // Else modify only the quantity of the product
+      foundProduct.quantity += productOptions.quantity;
+      localStorage.setItem("sofa", JSON.stringify(productInLocalStorage));
+    }
+  }
+}
+
+/**
+ * Onclick AddToCart button, add the product sofa in localStorage
+ */
+document
+  .getElementById("addToCart")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    let sofa = {
+      id: idProduct,
+      color: document.getElementById("colors").value,
+      quantity: parseInt(document.getElementById("quantity").value),
+      price: document.getElementById("price").innerText,
+    };
+    saveInLocalStorage(sofa);
+  });
--- front/js/script.js
+++ front/js/script.js
@@ -1,4 +1,8 @@
-/* GET data products from API */
+/**
+ * GET data products from API
+ * @async request
+ * @return { promise } which is parse/translate into JS in the function
+ */
 async function getProducts() {
   const urlProducts = `http://localhost:3000/api/products/`;
   const products = await fetch(urlProducts);
@@ -6,38 +10,42 @@ async function getProducts() {
   return productParse;
 }
 
+/**
+ * @async to get data product from API via @function getProducts
+ * Displaying all product's data on the page
+ */
 (async function main() {
   try {
     const products = await getProducts();
     console.log(products);
 
-    // creation card applying each product of products array
+    // Creation card applying each product of products array
 
     for (let product of products) {
-      // creation <a>
+      // Creation <a>
 
       const createA = document.createElement("a");
       createA.setAttribute("href", `./product.html?id=${product._id}`);
       createA.setAttribute("class", "linkProductPage");
       document.getElementById("items").appendChild(createA);
 
-      // creation <article>
+      // Creation <article>
       const createArticle = document.createElement("article");
       createA.appendChild(createArticle);
 
-      // creation <img>
+      // Creation <img>
       const createImg = document.createElement("img");
       createImg.setAttribute("src", product.imageUrl);
       createImg.setAttribute("alt", product.altTxt);
       createArticle.appendChild(createImg);
 
-      // creation <h3>
+      // Creation <h3>
       const createH3 = document.createElement("h3");
       createH3.setAttribute("class", "productName");
       createH3.innerText = product.name;
       createArticle.appendChild(createH3);
 
-      // creation <p>
+      // Creation <p>
       const createP = document.createElement("p");
       createP.setAttribute("class", "productDescription");
       createP.innerText = product.description;
